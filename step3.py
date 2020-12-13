7cbd6b70982656421eb26742c4dcf59a6a314790
G1-kang <59190912+G1-kang@users.noreply.github.com>
2020-12-13 22:50:08 +0900

    루빅스큐브 기본 배열구조

---
+++ step3.py
@@ -0,0 +1,9 @@
+cube = [[] for _ in range(6)]
+for _ in range(3):
+    cube[0].append(['O','O','O']) # 앞면
+    cube[1].append(['R','R','R']) # 밑면
+    cube[2].append(['B','B','B']) # 윗면
+    cube[3].append(['w','w','w']) # 왼쪽면
+    cube[4].append(['G','G','G']) # 오른쪽면
+    cube[5].append(['y','y','y']) # 후면
+
